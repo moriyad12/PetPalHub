a22b6e5a79ff55689abc94a35ace44e68b0c16dd
moriyad12 <mohamedelhenawy2012@yahoo.com>
2023-12-25 01:58:49 +0200

    added repositories

---
+++ src/main/java/com/example/PetPalHub/Repositories/LocationRepository.java
@@ -0,0 +1,8 @@
+package com.example.PetPalHub.Repositories;
+
+import com.example.PetPalHub.Entities.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+}
---
+++ src/main/java/com/example/PetPalHub/Repositories/ShelterRepository.java
@@ -0,0 +1,8 @@
+package com.example.PetPalHub.Repositories;
+
+import com.example.PetPalHub.Entities.Shelter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShelterRepository extends JpaRepository<Shelter, Integer> {
+
+}
