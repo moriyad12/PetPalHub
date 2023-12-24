f5498955e1f0ba26d4a145dd1b9e80a6e7fa1c2d
Ahmed Hassan <ahmedmohamedhassan1to10@gmail.com>
2023-12-24 20:53:29 +0200

    first

---
+++ src/main/java/com/example/PetPalHub/PetPalHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetPalHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPalHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PetPalHub/PetPalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetPalHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
