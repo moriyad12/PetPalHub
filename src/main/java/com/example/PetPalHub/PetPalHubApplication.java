96dbd9d027918985bd7ab8e601ee2d821cc33143
faris <113785816+faris0007@users.noreply.github.com>
2023-12-25 19:26:03 +0200

    modify Test Classes

--- src/main/java/com/example/PetPalHub/PetPalHubApplication.java
+++ src/main/java/com/example/PetPalHub/PetPalHubApplication.java
@@ -29,48 +29,7 @@ public class PetPalHubApplication {
     public CommandLineRunner commandLineRunner(AdopterRepositoryService adopterRepos, PetRepositoryService petRepositoryService,
                                                ApplicationRepository applicationRepository, ShelterRepository shelterRepository) {
         return runner -> {
-//            Adopter adopter = Adopter.builder()
-//                    .firstName("Adopter")
-//                    .lastName("Test")
-//                    .email("adopter" + "@test.com")
-//                    .phoneNumber("1234567890")
-//                    .gender(Gender.MALE)
-//                    .role(Role.ADOPTER)
-//                    .password("password")
-//                    .build();
-//            adopterRepos.add(adopter);
-//
-//
-//            Shelter shelter = Shelter.builder()
-//                    .name("abu ryad")
-//                    .shelterLocation(Location.builder().country("bianky").city("paradise").build())
-//                    .build();
-//            shelterRepository.save(shelter);
-//            Pet pet = Pet.builder()
-//                    .name("faris")
-//                    .breed(Breed.BEAGLE)
-//                    .availability(Availability.AVAILABLE)
-//                    .dateOfBirth(new Date())
-//                    .gender(Gender.FEMALE)
-//                    .healthStatus(HealthStatus.HEALTHY)
-//                    .behaviour(Behaviour.FRIENDLY)
-//                    .vaccineStatus(VaccineStatus.VACCINATED)
-//                    .description("cute")
-//                    .species("dog")
-//                    .shelter(shelter)
-//                    .build();
-//
-//
-//            petRepositoryService.addPet(pet);
-//            adopterRepos.add(adopter);
-//            AdopterPetApplication adopterPetApplication = new AdopterPetApplication(adopter, pet, Status.PENDING);
-//            applicationRepository.save(adopterPetApplication);
-//
-//            List<AdopterPetApplication> adopterPetApplicationList=applicationRepository.findByPet(pet);
-//            System.out.println();
-//
-//            List<AdopterPetApplication> adopterPetApplicationList2=applicationRepository.findByAdopter(adopter);
-//            System.out.println();
+
         };
     }
 }
--- src/test/java/com/example/PetPalHub/RepositoriesService/StaffRepositoryServiceTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.PetPalHub.RepositoriesService;
-
-public class StaffRepositoryServiceTest {
-}
