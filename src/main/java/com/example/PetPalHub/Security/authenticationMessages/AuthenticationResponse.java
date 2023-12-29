e1ca8a1ed98802d923fd5879270d4ab2a40e2888
faris <113785816+faris0007@users.noreply.github.com>
2023-12-29 23:47:09 +0200

    add ShelterId to proxy

--- src/main/java/com/example/PetPalHub/Security/authenticationMessages/AuthenticationResponse.java
+++ src/main/java/com/example/PetPalHub/Security/authenticationMessages/AuthenticationResponse.java
@@ -13,4 +13,5 @@ public class AuthenticationResponse {
     private String token;
     private int id;
     private String role;
+    private int shelterId;
 }
--- src/main/java/com/example/PetPalHub/Services/MasterControlServices.java
+++ src/main/java/com/example/PetPalHub/Services/MasterControlServices.java
@@ -90,4 +90,5 @@ public class MasterControlServices {
         }
         throw new RuntimeException();
     }
+
 }
